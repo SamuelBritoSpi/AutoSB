9267c85c2f2da8a8a2a9e622abdb5fc44b6df0bb
Samuel Brito <samuelbritosrr@gmail.com>
2025-09-02 14:29:02 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/components/Providers.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { ThemeProvider } from '@/components/ThemeProvider';
+import AuthProvider from '@/components/AuthProvider';
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="light"
+      enableSystem={false}
+      disableTransitionOnChange
+    >
+      <AuthProvider>
+        {children}
+      </AuthProvider>
+    </ThemeProvider>
+  );
+}
