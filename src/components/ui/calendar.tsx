951ed2946fd5f9e48b50e4579439536a45ac0662
Samuel Brito <samuelbritosrr@gmail.com>
2025-09-07 17:04:02 +0000

    Ainda não consigo ver visualmente o dia do calendario pintado de azul qu

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,14 +1,15 @@
+
 "use client"
 
 import * as React from "react"
 import { ptBR } from "date-fns/locale";
 import { ChevronLeft, ChevronRight } from "lucide-react"
-import { DayPicker } from "react-day-picker"
+import { DayPicker, type DayPickerProps, type ModifiersClassNames } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 
-export type CalendarProps = React.ComponentProps<typeof DayPicker>
+export type CalendarProps = DayPickerProps
 
 function Calendar({
   className,
