acf70b4511a36d9f75c5abd648b1f9c162734a2a
Samuel Brito <samuelbritosrr@gmail.com>
2025-09-07 16:16:34 +0000

    Try fixing this error: `Console Error: React expects the `children` prop

--- src/components/reports/ReportLayout.tsx
+++ src/components/reports/ReportLayout.tsx
@@ -14,7 +14,7 @@ export default function ReportLayout({ title, children }: ReportLayoutProps) {
     <html lang="pt-BR">
       <head>
         <meta charSet="UTF-8" />
-        <title>{title} - Relatório AutoSB</title>
+        <title>{`${title} - Relatório AutoSB`}</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
