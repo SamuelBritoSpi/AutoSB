4f6c423f2c9130c8dc342ce316b91a1f8704ba22
Samuel Brito <samuelbritosrr@gmail.com>
2025-09-08 22:10:35 +0000

    Quero PWA

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 
 import type {NextConfig} from 'next';
 
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
+
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
@@ -39,4 +47,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
