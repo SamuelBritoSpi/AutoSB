5398b2287155f77a4ac06e3e7bb68dc4ea90e928
Samuel Brito <samuelbritosrr@gmail.com>
2025-09-09 03:27:58 +0000

    Deixe agora o calendário responsivo, para diferentes tamanhos de tela

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -3,6 +3,7 @@
 
 import * as React from "react"
 import { ptBR } from "date-fns/locale";
+import { format } from "date-fns";
 import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker, type DayPickerProps, type ModifiersClassNames } from "react-day-picker"
 
@@ -22,6 +23,9 @@ function Calendar({
       locale={ptBR}
       showOutsideDays={showOutsideDays}
       className={cn("p-3", className)}
+      formatters={{
+        formatWeekdayName: (day) => format(day, 'EEEEEE', { locale: ptBR }),
+      }}
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
@@ -37,14 +41,13 @@ function Calendar({
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
         head_cell:
-          "text-muted-foreground rounded-md w-full font-normal text-[0.8rem]",
+          "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
-        cell: "size-full text-center text-sm p-0 relative first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+        cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
           buttonVariants({ variant: "ghost" }),
-          "h-full w-full p-0 font-normal aria-selected:opacity-100"
+          "h-9 w-9 p-0 font-normal aria-selected:opacity-100"
         ),
-        day_range_end: "day-range-end",
         day_selected:
           "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
         day_today: "bg-accent/50 text-accent-foreground",
@@ -67,4 +70,5 @@ function Calendar({
 Calendar.displayName = "Calendar"
 
 export { Calendar }
+
 
