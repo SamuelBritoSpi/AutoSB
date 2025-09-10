68ce3309e4c1e92ce94d66284dd877878dc0a7dd
Samuel Brito <samuelbritosrr@gmail.com>
2025-09-10 03:30:30 +0000

    Deu certo, mas você mexeu no tamanho do calendario da aba calendario, el

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -25,7 +25,7 @@ function Calendar({
       className={cn("p-3", className)}
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
-        month: "space-y-4 w-full",
+        month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
         caption_label: "text-sm font-medium",
         nav: "space-x-1 flex items-center",
@@ -36,7 +36,7 @@ function Calendar({
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
-        head_row: "flex w-full",
+        head_row: "flex",
         head_cell:
           "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
@@ -71,3 +71,4 @@ function Calendar({
 Calendar.displayName = "Calendar";
 
 export { Calendar };
+
